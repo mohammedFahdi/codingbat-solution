3cd4388964bcf4fd0e0b05e328f52527074bec20
Mohammed <malfahdi200@gmail.com>
2025-07-27 11:56:10 +0400

    front22

---
+++ front22.java
@@ -0,0 +1,9 @@
+public class front22 {
+    String str;
+    int take = 2;
+  if (take > str.length()) {
+    take = str.length();
+  }
+  String front = str.substring(0, take);
+  return front + str + front;
+}
