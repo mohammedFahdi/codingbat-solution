dc1fe6e6eabe9d50323dfcd3acf91821d5fbf4ce
Mohammed <malfahdi200@gmail.com>
2025-07-27 23:36:27 +0400

    frontTimes

---
+++ frontTimes.java
@@ -0,0 +1,13 @@
+public class frontTimes {
+int frontLen = 3;
+  if (frontLen > str.length()) {
+    frontLen = str.length();
+  }
+  String front = str.substring(0, frontLen);
+  String result = "";
+  for (int i=0; i<n; i++) {
+    result = result + front;
+  }
+  return result;
+}
+
