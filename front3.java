c7f1ba7db484b13df21c6ed1c88a4db533252c89
Mohammed <malfahdi200@gmail.com>
2025-07-27 11:33:49 +0400

    front3

---
+++ front3.java
@@ -0,0 +1,11 @@
+public class front3 {
+    String str;
+    String front;
+  if (str.length() >= 3) {
+    front = str.substring(0, 3);
+  } else {
+    front = str;
+  }
+ return front + front + front;
+}
+    
