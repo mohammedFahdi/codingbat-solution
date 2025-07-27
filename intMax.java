5936b928c16ac125c158d436cd363277170627a3
Mohammed <malfahdi200@gmail.com>
2025-07-27 12:46:34 +0400

    intMax

---
+++ intMax.java
@@ -0,0 +1,15 @@
+public class intMax {
+    int a; 
+    int b; 
+    int c;
+   int max;
+  if (a > b) {
+    max = a;
+  } else {
+    max = b;
+  }
+    if (c > max) {
+    max = c;
+  }
+  return max;
+}
