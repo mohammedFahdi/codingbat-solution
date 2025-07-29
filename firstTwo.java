b9c4dad4a0c77b8855f9b7af0f73587dcf4ba71c
Mohammed <malfahdi200@gmail.com>
2025-07-29 10:35:55 +0400

    firstTwo

---
+++ firstTwo.java
@@ -0,0 +1,9 @@
+public class firstTwo {
+    public String firstTwo(String str) {
+        if (str.length() < 2) {
+            return str;
+        } else {
+            return str.substring(0, 2);
+        }
+}
+}
