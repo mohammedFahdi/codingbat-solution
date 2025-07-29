9a1422d7e386c4c22dd7e39d66660f305b5f866c
Mohammed <malfahdi200@gmail.com>
2025-07-29 10:31:36 +0400

    makeOutWord

---
+++ makeOutWord.java
@@ -0,0 +1,6 @@
+public class makeOutWord {
+    public String makeOutWord(String out, String word) {
+        String start = out.substring(0, 2);  
+        String end = out.substring(2);  
+        return start + word + end;
+    }}
