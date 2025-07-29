c81d3911d5ead09ed26bcd01af94ac98dcd0e6dd
Mohammed <malfahdi200@gmail.com>
2025-07-29 12:08:43 +0400

    make2

---
+++ make2.java
@@ -0,0 +1,13 @@
+public class make2 {
+      int[] result = new int[2];
+        int count = 0;
+        for (int i = 0; i < a.length && count < 2; i++) {
+            result[count++] = a[i];
+        }
+        for (int i = 0; i < b.length && count < 2; i++) {
+            result[count++] = b[i];
+        }
+
+        return result;
+    
+}
