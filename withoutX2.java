82477569459d255856647be72327d9529cf15be2
Mohammed <malfahdi200@gmail.com>
2025-07-29 10:59:46 +0400

    withoutX2

---
+++ withoutX2.java
@@ -0,0 +1,15 @@
+public class withoutX2 {
+    public String withoutX2(String str) {
+    String result = "";
+        if (str.length() >= 1 && str.charAt(0) != 'x') {
+            result += str.charAt(0);
+        }
+        if (str.length() >= 2 && str.charAt(1) != 'x') {
+            result += str.charAt(1);
+        }
+        if (str.length() > 2) {
+            result += str.substring(2);
+        }
+        return result;
+    }    
+}
