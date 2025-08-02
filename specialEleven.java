3af706466dcef13bf2fec43fa7ae24c18eb1f87e
Mohammed <malfahdi200@gmail.com>
2025-08-02 12:28:52 +0400

    specialEleven

---
+++ specialEleven.java
@@ -0,0 +1,6 @@
+public class specialEleven {
+    public boolean specialEleven(int n) {
+    return (n % 11) == 0 || (n % 11) == 1;
+}
+    
+}
---
+++ squirrelPlay.java
@@ -0,0 +1,11 @@
+public class squirrelPlay {
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+    if(isSummer && 60 <= temp && temp <= 100)
+        return true;
+          
+    if(!isSummer && 60 <= temp && temp <= 90)
+        return true;
+                    
+    return false;
+}   
+}
