4df9e19db77dec7c4a36874386899452265c8fae
Mohammed <malfahdi200@gmail.com>
2025-08-03 11:24:29 +0400

    .

---
+++ wordsFront.java
@@ -0,0 +1,9 @@
+public class wordsFront {
+    public String[] wordsFront(String[] words, int n) {
+   String[] arr = new String[n];
+    
+    for(int i = 0; i < arr.length; i++)
+        arr[i] = words[i];
+              
+    return arr;
+}}
