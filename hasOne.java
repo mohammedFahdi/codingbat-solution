6ea007138f0d64fd07b7d4de8a84db4fd9ccf32e
Mohammed <malfahdi200@gmail.com>
2025-08-03 11:16:10 +0400

    .

---
+++ hasOne.java
@@ -0,0 +1,11 @@
+public class hasOne {
+    public boolean hasOne(int n) {
+   while(n != 0) {
+        if(n % 10 == 1)
+            return true;
+                  
+        n /= 10;
+    }
+                          
+    return false;
+}}
