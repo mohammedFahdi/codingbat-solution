44ef812647a90fcde0ced64d048b4896ac816c78
Mohammed <malfahdi200@gmail.com>
2025-08-03 11:09:43 +0400

    .

---
+++ bigHeights.java
@@ -0,0 +1,13 @@
+public class bigHeights {
+    public int bigHeights(int[] heights, int start, int end) {
+  int count = 0;
+    
+    for(int i = start; i < end; i++) {
+        if(Math.abs(heights[i] - heights[i+1]) >= 5)
+            count++;
+    }
+                    
+    return count;
+}
+    
+}
